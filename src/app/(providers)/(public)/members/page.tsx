79ab3366eb1d6b0df37b3b27bf17e4ba1dd6bf0b
eunbime <ebk1014@gmail.com>
2024-01-11 01:44:49 +0900

    [modify] 인재풀 페이지 ui 컴포넌트 분리

--- src/app/(providers)/(public)/members/page.tsx
+++ src/app/(providers)/(public)/members/page.tsx
@@ -1,23 +1,15 @@
 import React from "react"
 import MemberCard from "./_components/MemberCard"
 import Spacer from "@/components/ui/Spacer"
+import MemberCategory from "./_components/MemberCategory"
 
 const MembersPage = () => {
   return (
     <div>
       <Spacer y={90} />
       <div className="flex w-full">
-        {/* category */}
-        <ul className="flex flex-col px-[32px] py-[48px] w-[208px] h-[328px] gap-[20px] bg-gray-100 text-[17px]">
-          {/* TODO: isActive 시 bold 지정하기*/}
-          <li>전체보기</li>
-          <li>프론트엔드</li>
-          <li>백엔드</li>
-          <li>디자인</li>
-          <li>기획</li>
-        </ul>
+        <MemberCategory />
 
-        {/* member list */}
         <section className="flex flex-col ml-10 py-5 gap-[24px] w-full">
           <h3 className="text-[40px] font-[700]">전체보기</h3>
           <p className="text-[16px] font-[400] text-[#606060]">
