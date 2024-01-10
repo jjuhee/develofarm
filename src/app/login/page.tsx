299b19676ced52943b4eb7056c33a30fdffab061
boyaneck <jinxx93@naver.com>
2024-01-10 10:31:38 +0900

    [add] 로그인 폴더&페이지 생성

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -1,7 +1,17 @@
-import React from "react";
+import React from "react"
 
-const LoginPage = () => {
-  return <div>LoginPage</div>;
-};
+const page = () => {
+  return (
+    <>
+      <div>
+        <section className="flex flex-col justify-center items-center">
+          <div>소셜로그인1</div>
+          <div>소셜로그인2</div>
+          <div>소셜로그인3</div>
+        </section>
+      </div>
+    </>
+  )
+}
 
-export default LoginPage;
+export default page
