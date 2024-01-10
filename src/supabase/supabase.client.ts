0645ac97dd19fe9ffb156c0d9a983e86c64216a4
jjuhee <azaza.hee@gmail.com>
2024-01-10 11:25:29 +0900

    [modify] server/client 구분 export

--- src/supabase/supabase.client.ts
+++ src/supabase/supabase.client.ts
@@ -4,7 +4,7 @@ const supabaseUrl: string = process.env.NEXT_PUBLIC_SUPABASE_URL as string
 const supabaseServerKey: string = process.env
   .NEXT_PUBLIC_SUPABASE_CLIENT_SIDE_API_KEY as string
 
-const supabase = createClient(supabaseUrl, supabaseServerKey)
+const supabaseForClient = createClient(supabaseUrl, supabaseServerKey)
 
 // Export for usage by the rest of the app
-export { supabase }
+export { supabaseForClient }
