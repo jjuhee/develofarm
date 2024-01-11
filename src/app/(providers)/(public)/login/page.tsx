ebe08e5509cbd5bc791cd7fccce4304e6753f8e0
boyaneck <jinxx93@naver.com>
2024-01-11 10:43:07 +0900

    [add] 구글로그인 진행중,access denied 오류 제천 튜터님께 공유

--- src/app/(providers)/(public)/login/page.tsx
+++ src/app/(providers)/(public)/login/page.tsx
@@ -1,13 +1,36 @@
+"use client"
+import { supabaseForClient } from "@/supabase/supabase.client"
 import React from "react"
 
 const page = () => {
+  const getSocialGoogleSoicalLogin = () => {
+    const data = supabaseForClient.auth.signInWithOAuth({
+      provider: "google",
+      options: {
+        queryParams: {
+          access_type: "offline",
+          prompt: "consent",
+        },
+      },
+    })
+  }
+
   return (
     <>
       <div>
-        <section className="flex flex-col justify-center items-center">
-          <div>소셜로그인1</div>
-          <div>소셜로그인2</div>
-          <div>소셜로그인3</div>
+        <section className="flex flex-col justify-center items-center h-screen w-screen border">
+          <div
+            onClick={getSocialGoogleSoicalLogin}
+            className="mx-4 my-4 border w-60 text-center cursor-pointer hover:border-black "
+          >
+            구글 로그인
+          </div>
+          <div className="mx-4 my-4 border w-60 text-center cursor-pointer hover:border-black">
+            카카오 로그인
+          </div>
+          <div className="mx-4 my-4 border w-60 text-center cursor-pointer hover:border-black">
+            깃허브 로그인
+          </div>
         </section>
       </div>
     </>
