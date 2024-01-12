0956ee1714444a97d88fbdecc1abca6c7fe8d924
jjuhee <azaza.hee@gmail.com>
2024-01-12 23:34:03 +0900

    [modify] 글쓰기 페이지 프로젝트기간,구인인원 선택기능+ db에 저장

--- src/app/(providers)/(default)/write/_components/Tiptap.tsx
+++ src/app/(providers)/(default)/write/_components/Tiptap.tsx
@@ -2,26 +2,34 @@
 import { Color } from "@tiptap/extension-color"
 import ListItem from "@tiptap/extension-list-item"
 import TextStyle from "@tiptap/extension-text-style"
-import { BubbleMenu } from "@tiptap/react"
+import {
+  BubbleMenu,
+  EditorContentProps,
+  EditorContentState,
+} from "@tiptap/react"
 import { useEditor, EditorContent } from "@tiptap/react"
 import StarterKit from "@tiptap/starter-kit"
-import React, { useState } from "react"
+import React, { RefObject, useState } from "react"
 import EditorMenu from "./EditorMenu"
 import Spacer from "@/components/ui/Spacer"
 import BubbleMenuButtons from "./BubbleMenuButtons"
 
-const TipTap = () => {
-  const [contents, setContents] = useState<String>("")
+interface Props {
+  content: string
+  setContent: React.Dispatch<React.SetStateAction<string>>
+}
+
+const TipTap = ({ content, setContent }: Props) => {
   const editor = useEditor({
     extensions: extensions,
-    content: contents,
+    content: content,
     editorProps: {
       attributes: {
         class: "prose prose-sm max-w-none mx-auto focus:outline-none",
       },
     },
     onUpdate({ editor }) {
-      setContents(editor.getHTML())
+      setContent(editor.getHTML())
     },
   })
 
--- src/app/(providers)/(default)/write/page.tsx
+++ src/app/(providers)/(default)/write/page.tsx
@@ -1,12 +1,42 @@
 "use client"
 import Tiptap from "@/app/(providers)/(default)/write/_components/Tiptap"
 import Spacer from "@/components/ui/Spacer"
-import React, { useState } from "react"
+import { useMutation, useQueryClient } from "@tanstack/react-query"
+import React, { FormEvent, useRef, useState } from "react"
+import { addProject } from "./api"
+import { EditorContentProps } from "@tiptap/react"
 
 const Write = () => {
-  const [title, setTitle] = useState<string>()
+  const [title, setTitle] = useState<string>("")
+  const [content, setContent] = useState<string>("")
+  const [startDate, setStartDate] = useState<string>("")
+  const [endDate, setEndDate] = useState<string>("")
+  const [numberOfMembers, setNumberOfMembers] = useState<number>(0)
+  const queryClient = useQueryClient()
+  const { isPending, isError, error, mutate } = useMutation({
+    mutationFn: addProject,
+    onSuccess: () => {
+      queryClient.invalidateQueries()
+    },
+  })
+
+  const submitProjectHandler = (e: FormEvent) => {
+    e.preventDefault()
+    const dummyData = {
+      user_id: "51aa9a6d-70ee-4123-af62-c8831421d4cb", //TODO:(jhee) 임시
+      title,
+      content,
+      project_start_date: startDate,
+      project_end_date: endDate,
+      is_offline: false,
+      number_of_people: 2,
+    }
+    console.log(dummyData)
+    mutate(dummyData)
+  }
+
   return (
-    <div>
+    <form onSubmit={submitProjectHandler}>
       {/* TODO : (jhee) 첨부파일 넣는 곳? */}
       <div className="flex flex-col mt-10 mb-10">
         <div className="flex">
@@ -16,25 +46,44 @@ const Write = () => {
             onChange={(e) => setTitle(e.target.value)}
             className="grow border-black border-solid border-2 rounded-md " //TODO : 얇게 어떻게 햐
           />
-          <button className="w-40 rounded-full border-solid border-2 bg-black text-white">
+          <button
+            type="submit"
+            className="w-40 rounded-full border-solid border-2 bg-black text-white"
+          >
             작성하기
           </button>
         </div>
       </div>
       {/* TODO: (jhee) 선택 박스 추가, 위아래 border */}
-      <div className="border-solid border-y border-black">
+      <div className="flex flex-col border-solid border-y border-black">
         <Spacer y={30} />
-        <button>선택</button>
-        <button>버튼 투성이 으앙</button>
-        <button>버튼</button>
+        <input
+          type="date"
+          name="project_start_date"
+          value={startDate}
+          onChange={(e) => setStartDate(e.target.value)}
+        />
+
+        <input
+          type="date"
+          name="project_end_date"
+          value={endDate}
+          onChange={(e) => setEndDate(e.target.value)}
+        />
+        <label>인원 수</label>
+        <input
+          type="number"
+          value={numberOfMembers}
+          onChange={(e) => setNumberOfMembers(e.target.value)}
+        />
         <Spacer y={30} />
       </div>
       <Spacer y={30} />
       <div className="border-solid border-b border-black">
         <Spacer y={20} />
-        <Tiptap />
+        <Tiptap content={content} setContent={setContent} />
       </div>
-    </div>
+    </form>
   )
 }
 
