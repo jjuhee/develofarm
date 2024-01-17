3f4771b9d632138c73245eb2103c9ff941823d27
boyaneck <jinxx93@naver.com>
2024-01-17 21:30:10 +0900

    [add] 검색기능 완료,프로젝트만 검색되도록 추가로 구현하기

--- src/app/(providers)/(feature)/search/page.tsx
+++ src/app/(providers)/(feature)/search/page.tsx
@@ -1,7 +1,189 @@
-import React from "react"
+"use client"
+import React, { useEffect, useState } from "react"
+import { FaSearch } from "react-icons/fa"
+import { MdOutlineCancel } from "react-icons/md"
+//해당 이용자의 localstorage 가져오기
+interface keywordsInterface {
+  id?: number
+  num: number
+  text?: string
+}
 
+//
 const SearchPage = () => {
-  return <div>SearchPage</div>
+  const [keywords, setKeywords] = useState<keywordsInterface[]>([])
+  const [userId, setUserId] = useState<number>()
+  const [text, setText] = useState<string>()
+
+  //1.들어오자마자 로컬 스토리지의 keywords의 값이 있는지 확인하고 , 있으면 keywods state에 값을 넣는다
+  //2.keywords에 값이 있으면 map으로 돌려 ui에 보여준다
+  //3.검색버튼 클릭시 localstorage에 추가되고 ,keywords state에도 추가된다.
+
+  useEffect(() => {
+    //window 객체가 완전히 불려진 상태에서 localstorage를 확인하는 조건문(처음에만 들어옴)
+    if (typeof window !== "undefined") {
+      //로그인한 이용자의 id 추출하여 keywods의 id 값으로 들어감-----
+      const result2: any = localStorage.getItem(
+        "sb-aksbymviolrkiainilpq-auth-token",
+      )
+      const user_id = JSON.parse(result2).user.id
+      setUserId(user_id)
+      //------------------------------
+
+      //--만약 로컬스토리지에 keywords가 있다면 keywords state에 넣는다.
+      if (localStorage.getItem("keywords")) {
+        const result1: any = localStorage.getItem("keywords")
+        const json = JSON.parse(result1)
+        console.log("제이슨", json)
+        // setKeywords([json, ...keywords])
+        setKeywords(json)
+        console.log(
+          "새로고침하거나,새로들어오면 로컬스토리지에서 keywords를 state로 받아야한다?",
+          keywords,
+        )
+        //여기서 setKeywords를 해야하는데, 왜 자꾸 무한 loop됨?
+      }
+    }
+  }, [text])
+
+  //검색버튼 클릭하여 데이터(아이디,num,검색어) 추가
+  const onSubmitHandler = (event: React.FormEvent<HTMLFormElement>) => {
+    event.preventDefault()
+
+    const searchInput: string = event.currentTarget.search.value
+    if (searchInput === "") {
+      alert("검색어를 입력해주세요")
+      return
+    }
+
+    const newKeyword: keywordsInterface = {
+      id: userId,
+      num: keywords.length,
+      text: searchInput,
+    }
+
+    //화면에 보여지는 0~7중에 같은 검색어가 들어오면 검색 기록에 등재되지 않는다.
+    const isKeywordExist = keywords
+      .slice(0, 7)
+      .some((keyword) => keyword.text === newKeyword.text)
+    if (isKeywordExist) {
+      setText(searchInput)
+    } else {
+      setKeywords([newKeyword, ...keywords])
+      localStorage.setItem(
+        "keywords",
+        JSON.stringify([newKeyword, ...keywords]),
+
+        //여기서 api 진행할 것
+      )
+    }
+  }
+  //검색어 추가
+
+  // 단일 검색어 삭제
+  const onRemoveEachKeywordHandler = (num: number) => {
+    const nextKeyword = keywords?.filter((keyword) => {
+      return keyword.num !== num
+    })
+
+    setKeywords(nextKeyword)
+    localStorage.setItem("keywords", JSON.stringify(nextKeyword))
+  }
+  //검색어 기록  전체 삭제
+  const onRemoveAllKeywordsHnalder = () => {
+    setKeywords([])
+    localStorage.setItem("keywords", JSON.stringify([]))
+  }
+
+  // 검색어 삭제
+  const onRemoveTextHandler = () => {
+    setText("")
+  }
+
+  //검색기록 클릭시 해당 검색기록으로 검색창 텍스트 변환
+  const onSearchKeywordChangeHandler = (text: string) => {
+    setText(text)
+  }
+  return (
+    <>
+      <section className="flex justify-center align-center mt-40 ">
+        <form onSubmit={onSubmitHandler} className="relative">
+          <div className="relative flex items-center">
+            <button type="submit" className="absoulute top-3">
+              <FaSearch
+                size={20}
+                className="absolute left-3 top-4 text-gray-500 "
+              />
+            </button>
+            <input
+              className="p-3 pl-10 rounded-3xl border border-gray w-[30rem] focus:border-transparent focus:ring-2 focus:ring-green-300 transition-all duration-300"
+              type="text"
+              name="search"
+              value={text}
+              onChange={(e) => setText(e.target.value)}
+              placeholder="검색어를 입력해주세요"
+            ></input>
+            {text ? (
+              <span
+                onClick={onRemoveTextHandler}
+                className="absolute right-4 hover:cursor-pointer"
+              >
+                <MdOutlineCancel />
+              </span>
+            ) : (
+              <></>
+            )}
+          </div>
+        </form>
+      </section>
+      <section>
+        <div className="flex justify-center items-center">
+          <div className="flex justify-between mt-10 w-[500px]">
+            <h2 className="font-bold text-gray-700">최근 검색어</h2>
+            <button
+              className="hover:shadow-md hover:border-b rounded-xl "
+              onClick={onRemoveAllKeywordsHnalder}
+            >
+              전체삭제
+            </button>
+          </div>
+        </div>
+        {/* 로컬스토리지에서도 제한을 둘것? */}
+        <div className="flex justify-center">
+          <ul className=" flex flex-row w-[50rem] justify-center items-center mt-2 ">
+            {keywords?.length ? (
+              keywords.slice(0, 7).map((keyword) => (
+                <div
+                  className="bg-gray-200 rounded-3xl p-1 ml-2 relative hover:cursor-pointer "
+                  onClick={() =>
+                    onSearchKeywordChangeHandler(keyword.text as string)
+                  }
+                  key={keyword.id}
+                >
+                  <div className="bg-gray-200 rounded-3xl p-1 ml-2 relative flex items-center">
+                    <span className="flex-grow whitespace-nowrap overflow-hidden">
+                      {keyword.text}
+                    </span>
+                    <button
+                      onClick={() => onRemoveEachKeywordHandler(keyword.num)}
+                    >
+                      <span>
+                        <MdOutlineCancel />
+                      </span>
+                    </button>
+                  </div>
+                </div>
+              ))
+            ) : (
+              <>최근 검색어가 없습니다</>
+            )}
+          </ul>
+        </div>
+      </section>
+
+      <div>검색한 데이터 들어오는 곳 프로젝트와 인재풀!</div>
+    </>
+  )
 }
 
 export default SearchPage
