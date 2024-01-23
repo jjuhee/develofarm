4ea7fea388c230d85efeea45dcee58469ab864ea
KANG0417 <ipia99@naver.com>
2024-01-23 20:42:03 +0900

    [chores] 테일윈드 css 플러그인 스크롤바 추가

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -18,6 +18,6 @@ const config: Config = {
       },
     },
   },
-  plugins: [require("@tailwindcss/typography")],
+  plugins: [require("@tailwindcss/typography"), require("tailwind-scrollbar")],
 }
 export default config
