54f0194b5b7546d5ac42d814a270ac0951280523
boyaneck <jinxx93@naver.com>
2024-01-23 20:34:59 +0900

    칼럼 랜덤으로 사진과제목 내용 mvp1 버전 완성, but 고도화가 필

--- src/app/(providers)/(default)/(home)/_components/Columns.tsx
+++ src/app/(providers)/(default)/(home)/_components/Columns.tsx
@@ -14,8 +14,9 @@ interface props {
 const Column: React.FC<props> = ({ surfitArticles }: props) => {
   const [selectedImageNumber, setSelectedImageNumber] = useState<
     number | undefined
-  >()
+  >(undefined)
 
+  console.log("셀렉트 이미지 넘버", selectedImageNumber)
   const [surfitArticlesBundle, setSurfitArticlesBundle] = useState<
     SurfitArticles[] | undefined
   >()
@@ -42,16 +43,16 @@ const Column: React.FC<props> = ({ surfitArticles }: props) => {
           <div className="grid grid-cols-2 grid-rows-1 gap-x-2 h-full">
             {/* 왼쪽의 큰 이미지 */}
             <div className="relative overflow-hidden border border-black col-span-1 row-span-2 rounded-xl">
-              {selectedImageNumber !== undefined && (
+              {selectedImageNumber !== undefined ? (
                 <div className="w-full h-full flex flex-col opacity-100 transition-opacity duration-500">
                   <div className="flex-grow relative w-full h-full">
                     <Image
                       alt="대체이미지"
                       src={
                         surfitArticlesBundle
-                          ? surfitArticlesBundle[selectedImageNumber]?.imgSrc ||
-                            ""
-                          : "Loading..." // 또는 빈 문자열 또는 다른 로딩 처리 방식
+                          ? surfitArticlesBundle[selectedImageNumber + 1]
+                              ?.imgSrc
+                          : ""
                       }
                       layout="fill" // 부모 요소의 크기에 맞게 설정
                       objectFit="cover" // 이미지를 부모 요소에 맞춤
@@ -61,15 +62,47 @@ const Column: React.FC<props> = ({ surfitArticles }: props) => {
                     제목
                     {
                       surfitArticlesBundle
-                        ? surfitArticlesBundle[selectedImageNumber]?.title || ""
+                        ? surfitArticlesBundle[selectedImageNumber + 1]
+                            ?.title || ""
                         : "Loading..." // 또는 빈 문자열 또는 다른 로딩 처리 방식
                     }
                     <div>
                       내용
                       {
                         surfitArticlesBundle
-                          ? surfitArticlesBundle[selectedImageNumber]
+                          ? surfitArticlesBundle[selectedImageNumber + 1]
                               ?.description || ""
+                          : "Loading..." // 또는 빈 문자열 또는 다른 로딩 처리 방식
+                      }
+                    </div>
+                  </div>
+                </div>
+              ) : (
+                <div className="w-full h-full flex flex-col opacity-100 transition-opacity duration-500">
+                  <div className="flex-grow relative w-full h-full">
+                    <Image
+                      alt="대체이미지"
+                      src={
+                        surfitArticlesBundle
+                          ? surfitArticlesBundle[0]?.imgSrc
+                          : ""
+                      }
+                      layout="fill" // 부모 요소의 크기에 맞게 설정
+                      objectFit="cover" // 이미지를 부모 요소에 맞춤
+                    />
+                  </div>
+                  <div className="flex-shrink-0">
+                    제목
+                    {
+                      surfitArticlesBundle
+                        ? surfitArticlesBundle[0]?.title || ""
+                        : "Loading..." // 또는 빈 문자열 또는 다른 로딩 처리 방식
+                    }
+                    <div>
+                      내용
+                      {
+                        surfitArticlesBundle
+                          ? surfitArticlesBundle[0]?.description || ""
                           : "Loading..." // 또는 빈 문자열 또는 다른 로딩 처리 방식
                       }
                     </div>
@@ -82,7 +115,7 @@ const Column: React.FC<props> = ({ surfitArticles }: props) => {
               {/* 오른쪽의 4개 이미지 */}
 
               {/* {onGetsurfitArticlesRandom({surfitArticles)} */}
-              {surfitArticles.slice(0, 4).map((surfitArticle, index) => (
+              {surfitArticles.slice(1, 5).map((surfitArticle, index) => (
                 <div
                   key={index}
                   className="relative overflow-hidden border border-black col-span-1 row-span-1 rounded-xl"
