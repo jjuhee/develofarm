aadcf01555a112dc8ad41e7d0231eb31eee19bd8
boyaneck <jinxx93@naver.com>
2024-01-25 22:42:37 +0900

    next.config.js에 github 도메인 추가

--- src/app/(providers)/(default)/projects/api.ts
+++ src/app/(providers)/(default)/projects/api.ts
@@ -347,15 +347,14 @@ export async function getProjectsWithServer() {
   return data
 }
 
+type SupabaseResponse = {
+  data: any
+  error: Error | null
+}
+
 export async function getBookmarksCountEachProject() {
-  //   const sqlQuery = `
-  //   SELECT project_id, COUNT(*) as bookmarks_count
-  //   FROM bookmarks
-  //   GROUP BY project_id;
-  // `
-  //rpc 인자로 부르기
-  // const { data, error } = await supabaseForClient.rpc("hello_world")g
-  // if (error) console.log("error", error)
-  // console.log("북마크 갯수  프로젝트별로 새기!!!", data)
-  // return data
+  const { data, error } = await supabaseForClient.rpc("get_top_projects")
+  if (error) console.log("error", error)
+  console.log("북마크 갯수  프로젝트별로 count !!!@@@@@@", data)
+  return data
 }
