cf0a7cc996a54d0c267c5b1726cda0e999282771
boyaneck <jinxx93@naver.com>
2024-01-25 19:04:03 +0900

    [modify] api 북마크로직 주석처리

--- src/app/(providers)/(default)/projects/api.ts
+++ src/app/(providers)/(default)/projects/api.ts
@@ -411,13 +411,9 @@ export async function getBookmarksCountEachProject() {
   //   FROM bookmarks
   //   GROUP BY project_id;
   // `
-
   //rpc 인자로 부르기
-
-  // const { data, error } = await supabaseForClient.rpc("hello_world")
-
-  if (error) console.log("error", error)
-
-  console.log("북마크 갯수  프로젝트별로 새기!!!", data)
-  return data
+  // const { data, error } = await supabaseForClient.rpc("hello_world")g
+  // if (error) console.log("error", error)
+  // console.log("북마크 갯수  프로젝트별로 새기!!!", data)
+  // return data
 }
