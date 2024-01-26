1b5dd5b80caf6f8bb226a19d8caa59c836d19239
KANG0417 <ipia99@naver.com>
2024-01-26 11:57:20 +0900

    [modify] 상세페이지 날짜 수정

--- src/app/(providers)/(default)/write/_components/Editor.tsx
+++ src/app/(providers)/(default)/write/_components/Editor.tsx
@@ -115,6 +115,7 @@ const Editor = ({ projectId, project, techsWithPositions }: Props) => {
       is_offline: categoryData.isOffline!,
       number_of_people: categoryData.numberOfMembers,
       region_id: categoryData.region,
+      updated_at: isEditMode ? dayjs(new Date()).toString() : null,
     }
 
     mutate({
