5137ff6b6d36e7b3d619c2b8ac2446d2ec779838
jjuhee <azaza.hee@gmail.com>
2024-02-02 23:21:17 +0900

    [fix] 빌드에러 수정

--- src/app/(providers)/(default)/write/page.tsx
+++ src/app/(providers)/(default)/write/page.tsx
@@ -1,24 +1,27 @@
 "use client"
-import React, { useEffect } from "react"
+import React, { useEffect, useState } from "react"
 import Editor from "./_components/Editor"
 import { useRouter } from "next/navigation"
 import useLoginConfirmModal from "@/hooks/useLoginConfirmModal"
 
 const WritePage = () => {
-  const accessToken = localStorage.getItem(
-    process.env.NEXT_PUBLIC_AUTH_TOKEN as string,
-  )
   const openLoginConfirmModal = useLoginConfirmModal()
   const router = useRouter()
+  const [isLogin, setIsLogin] = useState(false)
 
   useEffect(() => {
+    const accessToken = localStorage.getItem(
+      process.env.NEXT_PUBLIC_AUTH_TOKEN as string,
+    )
     if (accessToken === null) {
       openLoginConfirmModal()
       router.replace("/")
+    } else {
+      setIsLogin(true)
     }
-  }, [accessToken])
+  }, [])
 
-  return <>{accessToken && <Editor />}</>
+  return <>{isLogin && <Editor />}</>
 }
 
 export default WritePage
