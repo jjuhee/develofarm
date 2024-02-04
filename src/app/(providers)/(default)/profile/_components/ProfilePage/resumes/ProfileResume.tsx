96b00d7e683acd541a09dae9255df665f84ba15a
HyoChang5147 <143697280+HyoChang5147@users.noreply.github.com>
2024-02-04 16:37:47 +0900

    [refactor]프로필 유저 페이지 api수정해서 props형식으로 리팩토링 하였습니다.

--- src/app/(providers)/(default)/profile/_components/ProfilePage/resumes/ProfileResume.tsx
+++ src/app/(providers)/(default)/profile/_components/ProfilePage/resumes/ProfileResume.tsx
@@ -3,17 +3,42 @@ import ProfileCareer from "./ProfileCareer"
 import ProfileEducation from "./ProfileEducation"
 import ProfileAcademy from "./ProfileAcademy"
 import ProfileSpec from "./ProfileSpec"
+import { useQuery } from "@tanstack/react-query"
+import { getUserResumes } from "../../../api"
+import { Tables } from "@/types/supabase"
 
 const ProfileResume = ({ profileId }: { profileId: string }) => {
+  const {
+    data: userResumes,
+    isLoading,
+    isError,
+  } = useQuery({
+    queryKey: ["userResumes", profileId],
+    queryFn: () => getUserResumes(profileId),
+    enabled: !!profileId,
+  })
+
+  if (isLoading) {
+    return <div className="hidden">Loading...</div>
+  }
+
+  if (isError) {
+    return <div>유저의 이력서 데이터를 불러오는 중 오류가 발생했습니다</div>
+  }
+
   return (
     <div>
       <div className="flex justify-between items-start">
-        <ProfileCareer profileId={profileId} />
-        <ProfileEducation profileId={profileId} />
+        <ProfileCareer careers={userResumes?.careers as Tables<"careers">[]} />
+        <ProfileEducation
+          educations={userResumes?.education as Tables<"education">[]}
+        />
       </div>
       <div className="flex justify-between items-start w-[1230px]">
-        <ProfileAcademy profileId={profileId} />
-        <ProfileSpec profileId={profileId} />
+        <ProfileAcademy
+          academies={userResumes?.academies as Tables<"academies">[]}
+        />
+        <ProfileSpec specs={userResumes?.specs as Tables<"specs">[]} />
       </div>
     </div>
   )
