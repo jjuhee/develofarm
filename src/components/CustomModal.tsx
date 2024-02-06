e6a4f1b39abc71492f2272488abe191cf902418f
KANG0417 <ipia99@naver.com>
2024-02-06 18:56:03 +0900

    [fix] 충돌 수정

--- src/components/CustomModal.tsx
+++ src/components/CustomModal.tsx
@@ -49,30 +49,7 @@ const CustomModal = () => {
     }
   }
 
-<<<<<<< HEAD
-  return mounted ? (
-    ReactDOM.createPortal(
-      <>
-        <div className="fixed top-0 left-0 right-0 bottom-0 flex bg-black bg-opacity-50 z-100" />
-        <div className="fixed top-[50%] left-[50%] flex transform: translate-x-[-50%] translate-y-[-50%] p-[50px] w-[450px] h-[260px] bg-white z-200 rounded-3xl">
-          <div className="w-full h-full flex flex-col justify-around items-center">
-            <h3 className="text-[20px] font-[600] whitespace-pre-line text-center">
-              {modalMessage}
-            </h3>
-            {isInput && (
-              <input
-                type="text"
-                placeholder="한줄 소개를 입력해주세요"
-                className="p-2"
-                maxLength={45}
-                value={input}
-                onChange={(e) => setInput(e.target.value)}
-                autoFocus
-              />
-            )}
-=======
   if (!viewCustomModal) return null
->>>>>>> f12edf04494873ff8bafad8424cd6342110de0ba
 
   if (!mounted) return <></>
 
