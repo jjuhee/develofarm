e551fcab50af777076ef9e2c6a816c0a1456fbf7
KANG0417 <ipia99@naver.com>
2024-02-21 16:44:54 +0900

    [add] 삭제 된 게시물 찾을 수 없는 페이지로 이동

--- src/app/(providers)/(default)/projects/[id]/page.tsx
+++ src/app/(providers)/(default)/projects/[id]/page.tsx
@@ -2,7 +2,7 @@
 
 import React, { useEffect } from "react"
 import Spacer from "@/components/ui/Spacer"
-import { useParams } from "next/navigation"
+import { notFound, useParams } from "next/navigation"
 import { useQuery } from "@tanstack/react-query"
 import { getProject } from "../api"
 import WriterEditRemoveButtons from "./_components/_header/WriterEditRemoveButtons"
@@ -20,7 +20,11 @@ import ThumbnailImageInfo from "./_components/_header/ThumbnailImageInfo"
 const DetailPage = () => {
   const { id } = useParams<{ id: string }>()
 
-  const { data: project, isLoading } = useQuery({
+  const {
+    data: project,
+    isLoading,
+    isError,
+  } = useQuery({
     queryKey: ["project", { projectId: id }],
     queryFn: () => getProject(id),
   })
@@ -46,52 +50,61 @@ const DetailPage = () => {
   const { user } = useUserStore((state) => state)
   const isWriter = user?.id === project?.user_id
 
-  if (isLoading || !project)
+  // 로딩중 이거나(||) project가 없거나 => 로딩중을 띄워줘
+  if (isLoading) {
     return (
       <div className="flex justify-center items-center h-[100vh]">
         <Image src={"/images/load.gif"} alt="load" width={200} height={200} />
       </div>
     )
+  }
+
+  // 로딩이 끝났어 그리고 project도 없어
+  if (isError || !project) {
+    return notFound()
+  }
 
   return (
-    <div className="flex flex-col w-full my-0 mx-auto">
-      <Spacer y={30} />
-      <header>
-        <Spacer y={50} />
-        <div className="flex items-center">
-          <span
-            className={`${
-              project.recruit_status
-                ? "bg-[#666666] border-[#666666] text-white"
-                : "bg-white  border-black text-black"
-            } min-w-[90px] px-3 py-1 border text-center rounded-3xl text-[16px] font-[600]`}
-          >
-            {project.recruit_status ? "모집 완료" : "모집 중"}
-          </span>
-          <h1 className="relative inline-flex items-center ml-3 w-[300px] sm:w-[500px] md:w-[700px] lg:w-[1030px] text-3xl font-semibold whitespace-pre-line break-words">
-            {project.title}
-            <ThumbnailImageInfo project={project} />
-          </h1>
-        </div>
+    project && (
+      <div className="flex flex-col w-full my-0 mx-auto">
         <Spacer y={30} />
-        <TechStackTag project={project} />
-        <Spacer y={25} />
-        <ul className="flex gap-x-5 pl-2 text-zinc-400 mb-5 items-center text-lg">
-          {project.user && <ProjectWriterInfo project={project} />}
-          <WriterEditRemoveButtons project={project} isWriter={isWriter} />
-        </ul>
-      </header>
-      <main>
-        <ProjectMetaInfo project={project} />
-        <Spacer y={50} />
-        <section className="mb-5 border-b-2 border-zinc-600 pb-10 min-h-96">
-          <EditorContent editor={editor} />
-        </section>
-        <FooterMenus project={project} />
-        <Spacer y={20} />
-      </main>
-      <FooterList />
-    </div>
+        <header>
+          <Spacer y={50} />
+          <div className="flex items-center">
+            <span
+              className={`${
+                project.recruit_status
+                  ? "bg-[#666666] border-[#666666] text-white"
+                  : "bg-white  border-black text-black"
+              } min-w-[90px] px-3 py-1 border text-center rounded-3xl text-[16px] font-[600]`}
+            >
+              {project.recruit_status ? "모집 완료" : "모집 중"}
+            </span>
+            <h1 className="relative inline-flex items-center ml-3 w-[300px] sm:w-[500px] md:w-[700px] lg:w-[1030px] text-3xl font-semibold whitespace-pre-line break-words">
+              {project.title}
+              <ThumbnailImageInfo project={project} />
+            </h1>
+          </div>
+          <Spacer y={30} />
+          <TechStackTag project={project} />
+          <Spacer y={25} />
+          <ul className="flex gap-x-5 pl-2 text-zinc-400 mb-5 items-center text-lg">
+            {project.user && <ProjectWriterInfo project={project} />}
+            <WriterEditRemoveButtons project={project} isWriter={isWriter} />
+          </ul>
+        </header>
+        <main>
+          <ProjectMetaInfo project={project} />
+          <Spacer y={50} />
+          <section className="mb-5 border-b-2 border-zinc-600 pb-10 min-h-96">
+            <EditorContent editor={editor} />
+          </section>
+          <FooterMenus project={project} />
+          <Spacer y={20} />
+        </main>
+        <FooterList />
+      </div>
+    )
   )
 }
 
